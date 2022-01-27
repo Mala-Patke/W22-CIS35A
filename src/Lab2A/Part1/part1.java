c1d2db97444e4501ef50370d59b7bfa09e7bfa3c
Mala-Patke <66596286+Mala-Patke@users.noreply.github.com>
2022-01-27 00:51:19 -0800

    Finish part 1 and start part 3

--- src/Lab2A/Part1/part1.java
+++ src/Lab2A/Part1/part1.java
@@ -1,15 +1,37 @@
 package Lab2A.Part1;
 
 public class part1 {
-	public static void decimalToBinary() {
+	public static final int decimalInput = 1101110;
+	public static final int[] binaryInput = {0, 1, 1, 1, 0, 1, 1}; //Read backwards
+	
+	public static int[] decimalToBinary(int decimal) {
+		int[] remainders = new int[(int) Math.floor(Math.log(decimal)/Math.log(2))+1];
+		int quotient = decimal;
+		int iter = 0;
 		
+		while(quotient != 0) {
+			remainders[iter++] = quotient % 2;
+			quotient /= 2;
+		}
+		
+		return remainders;
 	}
 	
-	public static void binaryToDecimal() {
-		
+	public static int binaryToDecimal(int[] binary) {
+		int result = 0;
+		for(int i = 0; i <= binary.length-1; i++) {
+			result += binary[i] * Math.pow(2, i);
+		}
+		return result;
 	}
 	
 	public static void main(String[] args) {
+		System.out.printf("%d in binary is ", decimalInput);
+		int[] d2b = decimalToBinary(decimalInput);
+		for(int i = d2b.length-1; i >= 0; i--) System.out.printf("%d", d2b[i]);
+		System.out.println("\n");
 		
+		for(int i = binaryInput.length-1; i >= 0; i--) System.out.printf("%d", binaryInput[i]);
+		System.out.printf(" in decimal is %d\n", binaryToDecimal(binaryInput));
 	}
 }
--- src/Lab2A/Part3/part3.java
+++ src/Lab2A/Part3/part3.java
@@ -1,5 +1,19 @@
 package Lab2A.Part3;
 
-public class part3 {
+import java.util.random.RandomGeneratorFactory;
 
+public class part3 {	
+	public static int[][] genTestData() {
+		int[][] result = new int[4][6];
+		for(int i = 0; i < result.length; i++) {
+			for(int j = 0; j < result[0].length; i++) {
+				
+			}
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
